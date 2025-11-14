65a0cabda948c7dc8c557703d113adb695f72f2e
Abuka13 <abukaozbekbay@gmail.com>
2025-11-14 17:58:13 +0500

    ShieldDecorator will reduce the damage of enemy

--- assignment67/src/Decorator/ShieldDecorator.java
+++ assignment67/src/Decorator/ShieldDecorator.java
@@ -1,4 +1,12 @@
 package Decorator;
 
-public class ShieldDecorator {
+import Hero.IHero;
+
+public class ShieldDecorator extends HeroDecorator {
+    public ShieldDecorator(IHero base) {
+        super(base);
+    }
+    public void takeDamage(int damage) {
+        base.takeDamage((int) (damage * 0.6));
+    }
 }
